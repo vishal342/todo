971b244b81e44265ee1fa9e6df0210dbb8eea90a
vishal342 <48347795+vishal342@users.noreply.github.com>
2020-09-19 16:15:11 +0530

    Updates

---
+++ precache-manifest.0e5f51b5ad7b7b9aa95332b6629785b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19da74c5c2f78686139b19bb5fe413e3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ee136e365bfed2a0c4eb",
+    "url": "/todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0ff465b311315956616",
+    "url": "/todo/static/js/2.79689e8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.79689e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee136e365bfed2a0c4eb",
+    "url": "/todo/static/js/main.ed5aed0d.chunk.js"
+  },
+  {
+    "revision": "5131de2ed6b872720718",
+    "url": "/todo/static/js/runtime-main.7c477364.js"
+  }
+]);
